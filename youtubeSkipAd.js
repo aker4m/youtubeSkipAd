f5d626277152215b3136095b29ade9dbbb89dc47
aker4m <mageaker@hotmail.com>
2021-12-08 15:08:10 +0900

    Update youtubeSkipAd.js

--- youtubeSkipAd.js
+++ youtubeSkipAd.js
@@ -1,3 +1,8 @@
 const run = (key, fn) => (e) => { if(e.key === key) fn() }
-const skipAdFn = () => { [".ytp-ad-skip-button", ".ytp-ad-overlay-close-button"].map(className => document.querySelector(className)).filter(el => el).forEach(el => el.click()) }
+const skipAdFn = () => { 
+  [".ytp-ad-skip-button", ".ytp-ad-overlay-close-button"]
+    .map(className => document.querySelector(className))
+    .filter(el => el)
+    .forEach(el => el.click()) 
+}
 document.addEventListener('keyup', run('s', skipAdFn))
